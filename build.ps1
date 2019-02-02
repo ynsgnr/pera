3e0c16b968c9e7d3b8c492c79e9487dcfce88560
Yunus Güngör <yunusgnr@gmail.com>
2019-02-03 00:00:44 +0300

    Added env value for flutter engine dll

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 mkdir .build
 flutter build bundle
 go run engineDownloader.go
+CMD /C set CGO_LDFLAGS=-L%cd% #taken from go-flutter-desktop-embedder
+$env:LIBRARY_PATH=(Get-Item -Path ".\").FullName #set the path for mingw to find flutter_engine.dll
 go build main.go
 .\main.exe
